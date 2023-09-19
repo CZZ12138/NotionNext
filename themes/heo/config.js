3290ce72995f12dc5682aa2a86c833314c794d8a
CZZ12138 <54562226+CZZ12138@users.noreply.github.com>
2023-09-19 16:29:21 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
     { title: '欢迎来到LUCEN', url: 'https://lucenczz.top' },
-    { title: '访问公告获取更多帮助', url: 'https://notion-next-ve9d.vercel.app/about' }
+    { title: '知能行考研数学帮助40万+考生找到秒杀考研数学的乐趣', url: 'https://bestzixue.com/?app_referrer_id=WBH' }
   ],
 
   // 英雄区(首页顶部大卡)
