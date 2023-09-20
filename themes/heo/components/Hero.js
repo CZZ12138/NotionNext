f144e1ab21f6d657f4cae6a265889fe3e4ae5e83
CZZ12138 <54562226+CZZ12138@users.noreply.github.com>
2023-09-20 15:56:47 +0800

    Update Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -347,7 +347,7 @@ function TodayCard({ cRef }) {
         className={`${
           isCoverUp
             ? 'opacity-100 cursor-pointer'
-            : 'opacity-0 transform scale-110 pointer-events-none'
+            : 'opacity-100 transform scale-110 pointer-events-none'
         } shadow transition-all duration-200 today-card h-full  rounded-xl relative overflow-hidden flex items-end`}
       >
         <div
